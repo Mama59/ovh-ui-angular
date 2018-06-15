69d61d6f557a24e036d89d8652f26b05422bbb44
Axel Peter <axel.peter@corp.ovh.com>
2018-06-15 11:34:26 +0200

    feat: add export default on modules

--- packages/oui-action-menu/src/index.js
+++ packages/oui-action-menu/src/index.js
@@ -2,7 +2,9 @@ import ActionMenu from "./action-menu.component.js";
 import ActionMenuDivider from "./action-menu-divider.component.js";
 import ActionMenuItem from "./action-menu-item.component.js";
 
-angular.module("oui.action-menu", [])
+export default angular
+    .module("oui.action-menu", [])
     .component("ouiActionMenu", ActionMenu)
     .component("ouiActionMenuDivider", ActionMenuDivider)
-    .component("ouiActionMenuItem", ActionMenuItem);
+    .component("ouiActionMenuItem", ActionMenuItem)
+    .name;
--- packages/oui-dropdown/src/index.js
+++ packages/oui-dropdown/src/index.js
@@ -2,7 +2,9 @@ import Dropdown from "./dropdown.component.js";
 import DropdownContent from "./dropdown-content.directive.js";
 import DropdownTrigger from "./dropdown-trigger.directive.js";
 
-angular.module("oui.dropdown", [])
+export default angular
+    .module("oui.dropdown", [])
     .component("ouiDropdown", Dropdown)
     .directive("ouiDropdownContent", DropdownContent)
-    .directive("ouiDropdownTrigger", DropdownTrigger);
+    .directive("ouiDropdownTrigger", DropdownTrigger)
+    .name;
